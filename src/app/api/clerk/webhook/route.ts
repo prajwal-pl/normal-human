1370afee866b59e3e1c98fd8e4f5672275245362
Prajwal-PL <prajwalpl096@gmail.com>
2024-10-07 19:45:15 +0530

    update webhook api

--- src/app/api/clerk/webhook/route.ts
+++ src/app/api/clerk/webhook/route.ts
@@ -1,6 +1,18 @@
-export async function POST(req: Request) {
+import { db } from "~/server/db";
+
+export const POST = async (req: Request) => {
   const { data } = await req.json();
-  console.log(data);
+  const emailAddress = data.email_addresses[0].email_address;
+  const firstName = data.first_name;
+  const lastName = data.last_name;
+  const imageUrl = data.image_url;
+  const id = data.id;
+
+  await db.user.upsert({
+    where: { id },
+    update: { emailAddress, firstName, lastName, imageUrl },
+    create: { id, emailAddress, firstName, lastName, imageUrl },
+  });
 
   return new Response("Webhook received", { status: 200 });
-}
+};
